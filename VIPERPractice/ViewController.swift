48e7c69ee93219a70b49d300b4d51f0c2d23a7c2
immanaro2497 <immanuel2497@gmail.com>
2025-01-06 17:03:21 +0530

    Initial Commit

---
+++ VIPERPractice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  VIPERPractice
+//
+//  Created by Immanuel on 06/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
